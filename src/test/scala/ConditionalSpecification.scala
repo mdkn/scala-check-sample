9256fb80970d2d73355c746ecce49400c312779d
mdkn <m.kan0842024@gmail.com>
2025-03-29 11:52:19 +0900

    add conditional spec

--- src/test/scala/ConditionalSpecification.scala
+++ src/test/scala/ConditionalSpecification.scala
@@ -1,7 +1,20 @@
 import org.scalacheck.Prop.{forAll, propBoolean}
+import org.scalacheck.{Gen, Prop, Properties}
 
-object ConditionalSpecification {
-  val propMakeList = forAll { (n: Int) =>
+object ConditionalSpecification extends Properties("Conditional") {
+  property("propReverseList") = forAll { (l: List[String]) => l.reverse.reverse == l }
+
+  property("propConcatString") = forAll { (s1: String, s2: String) =>
+    (s1 + s2).endsWith(s2)
+  }
+
+  val smallInteger = Gen.choose(0,100)
+
+  property("propSmallInteger") = Prop.forAll(smallInteger) { n =>
+    n >= 0 && n <= 100
+  }
+
+  property("propMakeList") = forAll { (n: Int) =>
     (n >= 0 && n < 10000) ==> (List.fill(n)("").length == n)
   }
 }
